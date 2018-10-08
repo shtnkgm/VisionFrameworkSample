16a78e152af8f1c17fdb35adebd53a2f5f1e67ec
shtnkgm <nakagami.shota@gmail.com>
2018-10-08 22:00:36 +0900

    support Swift4.2

--- VisionFrameworkSample/ViewController.swift
+++ VisionFrameworkSample/ViewController.swift
@@ -27,23 +27,31 @@ class ViewController: UIViewController {
     // カメラキャプチャの開始
     private func startCapture() {
         let captureSession = AVCaptureSession()
-        captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+        captureSession.sessionPreset = .photo
         
         // 入力の指定
-        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
-        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
-        guard captureSession.canAddInput(input) else { return }
+        guard let captureDevice = AVCaptureDevice.default(for: .video),
+            let input = try? AVCaptureDeviceInput(device: captureDevice),
+            captureSession.canAddInput(input) else {
+                assertionFailure("Error: 入力デバイスを追加できませんでした")
+                return
+        }
+        
         captureSession.addInput(input)
         
         // 出力の指定
-        let output: AVCaptureVideoDataOutput = AVCaptureVideoDataOutput()
+        let output = AVCaptureVideoDataOutput()
         output.setSampleBufferDelegate(self, queue: DispatchQueue(label: "VideoQueue"))
-        guard captureSession.canAddOutput(output) else { return }
+        
+        guard captureSession.canAddOutput(output) else {
+            assertionFailure("Error: 出力デバイスを追加できませんでした")
+            return
+        }
         captureSession.addOutput(output)
         
         // プレビューの指定
-        guard let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession) else { return }
-        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = .resizeAspectFill
         previewLayer.frame = view.bounds
         view.layer.insertSublayer(previewLayer, at: 0)
         
@@ -53,13 +61,18 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
-    func captureOutput(_ output: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, from connection: AVCaptureConnection!) {
-        
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         // CMSampleBufferをCVPixelBufferに変換
-        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            assertionFailure("Error: バッファの変換に失敗しました")
+            return
+        }
         
         // CoreMLのモデルクラスの初期化
-        guard let model = try? VNCoreMLModel(for: Resnet50().model) else { return }
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else {
+            assertionFailure("Error: CoreMLモデルの初期化に失敗しました")
+            return
+        }
         
         // 画像認識リクエストを作成（引数はモデルとハンドラ）
         let request = VNCoreMLRequest(model: model) {
@@ -68,7 +81,7 @@ extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
             
             // 判別結果とその確信度を上位3件まで表示
             // identifierは類義語がカンマ区切りで複数書かれていることがあるので、最初の単語のみ取得する
-            let displayText = results.prefix(3).flatMap { "\(Int($0.confidence * 100))% \($0.identifier.components(separatedBy: ", ")[0])" }.joined(separator: "\n")
+            let displayText = results.prefix(3).compactMap { "\(Int($0.confidence * 100))% \($0.identifier.components(separatedBy: ", ")[0])" }.joined(separator: "\n")
             
             DispatchQueue.main.async {
                 self?.textView.text = displayText
